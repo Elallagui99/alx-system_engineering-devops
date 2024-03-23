2e2346d7850b0ba69cc3b0601effdeff47f4cea1
Elallagui99 <got.famme@gmail.com>
2024-03-23 23:26:23 +0000

    fixing

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process nemed killmenow
 
-exec { 'pkill killmenow' :
-  path    => /bin/,
-  command => 'pkill killmenow',
+exec { 'kill' :
+  path    => 'usr/bin/',
+  command => 'pkill -f killmenow',
 }
